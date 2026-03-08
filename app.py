ab4c134475306592c53faab051f99b395c077620
Shaik Sohel <thisissohel07@gmail.com>
2026-03-08 15:45:53 +0530

    fix render start command

--- app.py
+++ app.py
@@ -593,7 +593,8 @@ def download_file(filename):
     return send_from_directory(DOWNLOAD_DIR, safe_name, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 # from flask import Flask, request, jsonify, render_template_string, send_file
